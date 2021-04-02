8bdde10d414df57cc26eaec2592742ac2b7ac9cc
Md. Rishad Khan <rishadkhan190@gmail.com>
2021-04-02 23:21:42 +0600

    done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Spinner } from "react-bootstrap";
 import BookData from "../BookData/BookData";
 import "./Home.css";
 
 const Home = () => {
   const [booksData, setbooksData] = useState([]);
+  const [loading, setLoading] = useState();
 
   useEffect(() => {
     fetch("https://rocky-savannah-92763.herokuapp.com/showdata")
@@ -14,17 +15,24 @@ const Home = () => {
       .then((data) => {
         setbooksData(data);
       });
+    setLoading(true);
   }, []);
   return (
     <div>
       <Container>
         <Row>
           <Col>
-            <div className="box_books">
-              {booksData.map((book) => (
-                <BookData key={book._id} bookdata={book}></BookData>
-              ))}
-            </div>
+            {loading ? (
+              <div className="box_books">
+                {booksData.map((book) => (
+                  <BookData key={book._id} bookdata={book}></BookData>
+                ))}
+              </div>
+            ) : (
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            )}
           </Col>
         </Row>
       </Container>
